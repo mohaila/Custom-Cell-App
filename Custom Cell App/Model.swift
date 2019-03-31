dd33c0cb2431129b576d3bd8a63eb61a111e600c
mohaila <mohamed.ilane@gmail.com>
2019-03-30 23:50:42 -0400

    Add option menu for selected row

--- Custom Cell App/Model.swift
+++ Custom Cell App/Model.swift
@@ -17,4 +17,6 @@ class Model {
     static let restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
     
     static let restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    static var restaurantCheckin = Array(repeating: false, count: 21)
 }
--- Custom Cell App/ViewController.swift
+++ Custom Cell App/ViewController.swift
@@ -33,8 +33,46 @@ class ViewController: UITableViewController {
         cell.locationLabel.text = Model.restaurantLocations  [indexPath.row]
         cell.typeLabel.text = Model.restaurantTypes  [indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: Model.restaurantImages  [indexPath.row])
+        cell.accessoryType = Model.restaurantCheckin [indexPath.row] ? .checkmark : .none
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        let callActionHandler = {(action:UIAlertAction)->Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call (800)123-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        if Model.restaurantCheckin [indexPath.row] {
+            let undoCheckinHandler = {(action:UIAlertAction)->Void in
+                if Model.restaurantCheckin [indexPath.row] {
+                    let cell = tableView.cellForRow(at: indexPath)
+                    cell?.accessoryType = .none
+                    Model.restaurantCheckin [indexPath.row] = false
+                }
+            }
+            let undoCheckinAction = UIAlertAction(title: "Undo Check in", style: .default, handler: undoCheckinHandler)
+            optionMenu.addAction(undoCheckinAction)
+        } else {
+            let checkinHandler = {(action:UIAlertAction)->Void in
+                let cell = tableView.cellForRow(at: indexPath)
+                cell?.accessoryType = .checkmark
+                Model.restaurantCheckin [indexPath.row] = true
+            }
+            let checkinAction = UIAlertAction(title: "Check in", style: .default, handler: checkinHandler)
+            optionMenu.addAction(checkinAction)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        present(optionMenu, animated: true, completion: nil)
     }
 
 }
